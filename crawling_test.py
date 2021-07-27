e34b50482b6782f3783af9c08c7c97ab26c145c2
ycyycy0108-git <69133717+ycyycy0108-git@users.noreply.github.com>
2021-07-27 15:34:22 +0900

    Update crawling_test.py

--- crawling_test.py
+++ crawling_test.py
@@ -4,10 +4,19 @@
 final_project
 
 1. data crawling
+
+
 """
 
 import urllib.request
 import urllib.parse
+import requests
 from bs4 import BeautifulSoup
+
+res = requests.get("http://google.com")
+res.raise_for_status()
+print('웹 연결 성공...')
+#문제 생기면 여기서 바로 끝남! 쓸데없는 시간 낭비를 줄여줌
+
 
 #tnwjd
