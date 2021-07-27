ec87d4fd2ba2bc6d9a09a0def8b76ab1f95114b3
OH JI AE <77822999+aegohc@users.noreply.github.com>
2021-07-27 15:29:07 +0900

    Add files via upload

---
+++ crawling_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 27 11:17:27 2021
+
+@author: aegohc
+"""
+
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
