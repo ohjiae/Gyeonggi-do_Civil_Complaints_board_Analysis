0d1bc82b5e4dfe6330fb0ac6a5ce156338873185
OH JI AE <77822999+aegohc@users.noreply.github.com>
2021-07-28 15:17:59 +0900

    Update complaints_board.py
    주석정리

--- complaints_board.py
+++ complaints_board.py
@@ -8,39 +8,34 @@ Final project : complaints analysis
 4. modeling
 5. evaluating
 """
-
+# 모듈 import (라이브러리에서 사용하는 모듈만 호출)
 # selenum 사용
-from selenium import webdriver # 라이브러리에서 사용하는 모듈만 호출
+from selenium import webdriver
 import os 
 from bs4 import BeautifulSoup
 import requests
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By  # 기다리는 조건 설정 시 탐색용 
+from selenium.webdriver.support.ui import WebDriverWait  # 기다리는 용도 (비동기)
+from selenium.webdriver.support import expected_conditions as EC  # 기다릴 때의 조건 설정용
+from selenium.webdriver.common.keys import Keys  # 키 입력용
 
 
-
+# 경로 확인
 pwd = os.getcwd() # 현재 경로 
 print(pwd)
 
 
 
 #def complain_crawler(name) : 
-   # 1. dirver 경로/파일 지정 
-    driver = webdriver.Chrome("/Users/aegohc/ITWILL/final_project/chromedriver") # 각자 컴터에 맞게 수정!
+# 1. dirver 경로/파일 지정 
+driver = webdriver.Chrome("/Users/aegohc/ITWILL/final_project/chromedriver") # 각자 컴터에 맞게 수정!
     
-    # 2. 민원 검색 url 
-    driver.get("https://www.epeople.go.kr/nep/pttn/gnrlPttn/pttnSmlrCaseList.npaid") # 민원을 검색할 수 있는 페이지
+# 2. 민원 검색 url 
+driver.get("https://www.epeople.go.kr/nep/pttn/gnrlPttn/pttnSmlrCaseList.npaid") # 민원을 검색할 수 있는 페이지
 
     
-    # 3. div Element 가져오기
-    #selectJungAng = driver.find_element_by_css_selector("#instListDiv > option:nth-child(34)").click()
-
-    #selectJungAng = Select(driver.find_element_by_xpath("//*[@id="instListDiv"]"))
-    
-
+# 3. div Element 가져오기
 
 current_total_pages = driver.find_element_by_xpath('//*[@id="frm"]/div[2]/span/span').text #현재 총 검색결과 수 
 print(current_total_pages)
@@ -59,5 +54,6 @@ driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div[1]/div[4]/button[1]').se
                        
 #push_srch_bt = driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div[1]/div[4]/button[1]').submit # 검색을 누릅니다
 
-                    
-                       
+
+# 4. Crawling
+
