42bbc3f8a387cb71563b2d4b7819501d416dadce
DahyeonS <88021405+DahyeonS@users.noreply.github.com>
2021-07-28 17:52:25 +0900

    Add files via upload

--- complaints_board.py
+++ complaints_board.py
@@ -213,3 +213,68 @@ for title in titles :
     
 
     
+# 서다현 2021-07-28 17:50
+from selenium import webdriver
+import os 
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By  # 기다리는 조건 설정 시 탐색용 
+from selenium.webdriver.support.ui import WebDriverWait  # 기다리는 용도 (비동기)
+from selenium.webdriver.support import expected_conditions as EC  # 기다릴 때의 조건 설정용
+from selenium.webdriver.common.keys import Keys  # 키 입력용
+
+
+# 경로 확인
+pwd = os.getcwd() # 현재 경로 
+print(pwd)
+
+
+
+#def complain_crawler(name) : 
+# 1. dirver 경로/파일 지정 
+#driver = webdriver.Chrome("/Users/aegohc/ITWILL/final_project/chromedriver") # 각자 컴터에 맞게 수정!
+driver = webdriver.Chrome("C:/Users/sdh04/OneDrive/바탕 화면/ITWILL-Final_project-main/chromedriver")
+    
+# 2. 민원 검색 url 
+driver.get("https://www.epeople.go.kr/nep/pttn/gnrlPttn/pttnSmlrCaseList.npaid") # 민원을 검색할 수 있는 페이지
+
+    
+# 3. div Element 가져오기
+
+current_total_pages = driver.find_element_by_xpath('//*[@id="frm"]/div[2]/span/span').text #현재 총 검색결과 수 
+print(current_total_pages)
+
+
+driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div[2]/a[2]/span').click()#상세 검색으로 늘려줍니다
+wait = WebDriverWait(driver, 10)
+wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="searchInstType"]/option[2]'))) #중앙행정기관 선택 가능할때까지 기다리기
+driver.find_element_by_xpath('//*[@id="searchInstType"]/option[2]').click() #중앙행정기관 선택
+
+wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="instListDiv"]/option[34]'))) #국세청 선택 가능할때까지 기다리기
+driver.find_element_by_xpath('//*[@id="instListDiv"]/option[34]').click() #국세청 선택
+
+
+driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div[1]/div[4]/button[1]').send_keys(Keys.ENTER) #검색 클릭이 안되서 엔터로 대신
+                       
+#push_srch_bt = driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div[1]/div[4]/button[1]').submit # 검색을 누릅니다
+
+driver.find_element_by_css_selector('#listCnt > option:nth-child(5)').click()
+
+
+# 4. Crawling
+for i in range(69) : 
+    titles = []
+    
+    driver.find_element_by_xpath('//*[@id="frm"]/div[3]/span[4]/a/img').click()
+
+    for i in range(50) : 
+
+        title = driver.find_element_by_css_selector('#frm > table > tbody > tr:nth-child(%d) > td.left > a'%(i+1)).text
+        titles.append(title)
+    
+    #print(titles)
+    
+len(titles)
+
+driver.close()
