45622d827e42ae22e6a4b5256357efe8aec170da
Dahyeon Seo <88021405+DahyeonS@users.noreply.github.com>
2021-08-06 10:57:32 +0900

    Update conplaints_preprocessing.py

--- conplaints_preprocessing.py
+++ conplaints_preprocessing.py
@@ -6,9 +6,10 @@ conplaints_preprocessing.py
 민원 전처리
 """
 ########### 주의 ##########
-# <base>에 Jpype를 설치한 경우,
-# <tensorflow> 환경에서는 konlpy 패키지가 작동하지 않을 수 있으므로
-# 반드시 <tensorflow>에 먼저 Jpype를 설치한 환경에서만 작동하시길 바랍니다.
+# <base>에 Jpype를 설치한 경우, <tensorflow> 환경에서는 
+# konlpy 패키지가 작동하지 않을 수 있으므로
+# 반드시 <tensorflow>에 먼저 Jpype를 설치한 환경에서만 작동하시거나
+# spyder를 anaconda3로 작동해주시길 바랍니다.
 
 
 # 0. package load
@@ -20,7 +21,7 @@ from konlpy.tag import Okt
 
 # 1. csv file load
 #path = 'K:/ITWILL/Final_project/'
-path = 'C:/Users/STU-16/Desktop/빅데이터/Final_Project/ITWILL-Final_project-main/'
+path = 'D:/서다현/빅데이터/Final_Project/ITWILL-Final_project-main/'
 minwon_data = pd.read_csv(path + 'crawlingdata17326.csv')
 minwon_data.info()
 '''
@@ -47,8 +48,8 @@ def text_prepro(titles):
     titles = [x.lower() for x in titles]
     # Remove punctuation : 문장부호 제거
     titles = [''.join(c for c in x if c not in string.punctuation) for x in titles]
-    # Remove numbers : 숫자 제거
-    titles = [''.join(c for c in x if c not in string.digits) for x in titles]
+    # Remove numbers : 숫자 제거 [생략]
+    #titles = [''.join(c for c in x if c not in string.digits) for x in titles]
     # Trim extra whitespace : 공백 제거
     titles = [' '.join(x.split()) for x in titles]
     return titles
@@ -60,7 +61,7 @@ print(titles)
 
 # 3. 불용어 제거 - Okt 함수 이용
 # 1) 불용어 사전 - https://www.ranks.nl/stopwords/korean
-korean_stopwords = "C:/Users/STU-16/Desktop/빅데이터/Final_Project/ITWILL-Final_project-main/korean_stopwords.txt"
+korean_stopwords = path + "/korean_stopwords.txt"
 
 with open(korean_stopwords, encoding='utf8') as f : 
     stopwords = f.readlines()
