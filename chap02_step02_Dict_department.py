13ac632342add229035bb77fb77ce5c97dcc6df0
OH JI AE <77822999+aegohc@users.noreply.github.com>
2021-08-09 19:49:05 +0900

    Update chap02_step02_Dict_department.py

--- chap02_step02_Dict_department.py
+++ chap02_step02_Dict_department.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 chap02_step02_Dict_department
 1. 부서(Key)와 관련 키워드(Value)를 가진 딕셔너리 만들기
@@ -8,15 +6,18 @@ chap02_step02_Dict_department
 
 
 top5_dept = {
-    '철도정책과' :,
-    '버스정책과' :,
-    '신도시기획과' :,
-    '총무과' :,
-    '신도시추진과' :,
+    '도로정책과' :['계획','다리','일산대교','착공','변경','이전','교통','면제','통행료','한강','대교','도로'],
+    '철도정책과' :['교통','북부','연장','착공','유치','신분당선','신설','역','면제','확정','지하철','호선','철도','노선'],
+    '버스정책과' :['교통','노선','변경','이전','교통카드','교통비','청소년','버스','대중교통','요금'],
+    '철도건설과' :['교통','변경','이전','착공','원안','위치','호선','역','지하철','철도','노선'],
+    '신도시추진단' :['시설','유치','이전','주택','지역','원안','위치','신도시'],
+    '총무과' :['이재명','도지사','도지사님','지사님']
+    
 }
 
-def find_dept(dict, key_value):
-  return list(key for key, value in dict.items() if value == key_value)
 
+def find_dept(keyword):
+    print(list(value for value in dict.values() if value == keyword))
+    
+print(find_dept('다리'))
 
-print(find_dept(top5_dept,'키워드값(value)'))
